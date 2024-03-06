6bb1e085b8961bce4fa52d4b98ab5c348d1d7c9f
pynat <natlu@gmx.de>
2024-03-06 09:52:25 +0100

    first pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,20 @@
 import pandas as pd
 
-data = pd.read_csv("Data.csv")
+csv_file_path = "Data.csv"
 
-print(data.head())
+df = pd.read_csv(csv_file_path)
+
+selected_columns = [
+    'Frame Colour',
+    'Frame Surface',
+    'Frame Material',
+    'Glazing Type',
+    'Size Horizontal [m]',
+    'Size Vertical [m]',
+    'Frame Depth [cm]'
+]
+
+
+df = df[selected_columns]
+
+print(df.head())
