6d9cda6fd56fcac0d7a61520befb330dcff8f3af
pynat <natlu@gmx.de>
2024-03-11 15:17:11 +0100

    adding functions

--- cell_calculate.py
+++ cell_calculate.py
@@ -1,10 +1,38 @@
 import json
 
-# Calculate Area  Window
+# Calculate Net Glass Area
+def calculate_net_glass_area(cell):
+    glass_area = cell["glas"]["width"] * cell["glas"]["hight"]
+    return glass_area
+
+# Calculate Area  Window(including frame and glass)
 def calculate_window_area(cell):
     frame_area = (cell["frame"]["width"] * cell["frame"]["hight"]) - (cell["frame"]["profile"]["width"] * cell["frame"]["profile"]["depth"])
     glass_area = cell["glas"]["width"] * cell["glas"]["hight"]
     return frame_area + glass_area
+
+# Calculate Frame Area
+def calculate_frame_area(cell):
+    frame_area = (cell["frame"]["width"] * cell["frame"]["hight"]) - (cell["frame"]["profile"]["width"] * cell["frame"]["profile"]["depth"])
+    return frame_area
+
+# Calculate Total Net Glass Area
+def calculate_total_net_glass_area(grid):
+    total_net_glass_area = 0
+
+    for cell in grid["cells"]:
+        total_net_glass_area += calculate_net_glass_area(cell)
+
+    return total_net_glass_area
+
+def calculate_total_frame_area(grid):
+    total_frame_area = 0
+
+    for cell in grid["cells"]:
+        total_frame_area += calculate_frame_area(cell)
+
+    return total_frame_area
+
 
 # Grid
 def calculate_total_window_area(grid):
@@ -28,5 +56,6 @@ total_heat_transfer = window_area * heat_transfer_coefficient
 
 print("Total Window Area:", window_area)
 print("Total Heat Transfer:", total_heat_transfer)
+print("Total Heat Transfer for Glass Area Only:", total_heat_transfer)
 
 
