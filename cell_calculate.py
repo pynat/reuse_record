bcc982bdd00e3208229fdfc87e52fa2a8778b1b2
pynat <natlu@gmx.de>
2024-03-11 08:51:51 +0100

    cell_calculate.py

---
+++ cell_calculate.py
@@ -0,0 +1,32 @@
+import json
+
+# Calculate Fläche Window
+def calculate_window_area(cell):
+    frame_area = (cell["frame"]["width"] * cell["frame"]["hight"]) - (cell["frame"]["profile"]["width"] * cell["frame"]["profile"]["depth"])
+    glass_area = cell["glas"]["width"] * cell["glas"]["hight"]
+    return frame_area + glass_area
+
+# Grid
+def calculate_total_window_area(grid):
+    total_area = 0
+
+    for cell in grid["cells"]:
+        total_area += calculate_window_area(cell)
+
+    return total_area
+
+
+with open("pfad/datei.json", "r") as file:
+    grid_data = json.load(file)
+
+# Constants
+window_area = calculate_total_window_area(grid_data)
+heat_transfer_coefficient = 0.8 #example
+
+# Calculate Heat transfer
+total_heat_transfer = window_area * heat_transfer_coefficient
+
+print("Total Window Area:", window_area)
+print("Total Heat Transfer:", total_heat_transfer)
+
+
